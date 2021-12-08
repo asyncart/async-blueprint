5f94d22e2e76cab76a5ad2e3d8a2fac44e8d2f5e
Conlan Rios <conlan@gmail.com>
2021-12-08 14:06:13 -0800

    Fix baseTokenURI to pull in blueprint specific base

--- contracts/contracts/Blueprint.sol
+++ contracts/contracts/Blueprint.sol
@@ -21,14 +21,13 @@ contract Blueprint is
     uint64 public latestErc721TokenIndex;
     uint256 public blueprintIndex;
 
-    string public baseTokenUri;
-
     address public asyncSaleFeesRecipient;
     address public platform;
     address public minterAddress;
-
-    mapping(uint256 => Blueprints) public blueprints;
+    
+    mapping(uint256 => uint256) tokenToBlueprintID;
     mapping(address => uint256) failedTransferCredits;
+    mapping(uint256 => Blueprints) public blueprints;
 
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
@@ -404,6 +403,8 @@ contract Blueprint is
         uint64 newCap = blueprints[_blueprintID].capacity;
         for (uint16 i = 0; i < _quantity; i++) {
             _mint(msg.sender, newTokenId + i);
+            tokenToBlueprintID[newTokenId + i] = _blueprintID;
+
             bytes32 prefixHash = keccak256(
                 abi.encodePacked(
                     block.number,
@@ -514,15 +515,8 @@ contract Blueprint is
         blueprints[blueprintID].tokenUriLocked = true;
     }
 
-    function setBaseTokenUri(string memory _baseTokenUri)
-        external
-        onlyRole(DEFAULT_ADMIN_ROLE)
-    {
-        baseTokenUri = _baseTokenUri;
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseTokenUri;
+    function _baseURIForBlueprint(uint256 tokenId) internal view returns (string memory) {
+        return blueprints[tokenToBlueprintID[tokenId]].baseTokenUri;
     }
 
     function tokenURI(uint256 tokenId)
@@ -537,7 +531,7 @@ contract Blueprint is
             "ERC721Metadata: URI query for nonexistent token"
         );
 
-        string memory baseURI = _baseURI();
+        string memory baseURI = _baseURIForBlueprint(tokenId);
         return
             bytes(baseURI).length > 0
                 ? string(
