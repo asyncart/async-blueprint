30e0df6b7a08c8a80340764811948f417bdb201d
sharthan <sharthan@users.noreply.github.com>
2022-06-15 13:44:09 +0200

    Revert "Proxy contract transparent"
    This reverts commit 5a0279e93883bc0d9d2fadc1134267a168042a0a.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
@@ -7,13 +6,12 @@ require("hardhat-deploy");
 require("@nomiclabs/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
 
-//Rinkeby & polygon test network
-//NB Etherscan and AlchemyUrl is for Polygon network!!
-const polygonPrivateKey = process.env.POLYGON_PRIVATE_KEY;
-const rinkebyPrivateKey = process.env.RINKEBY_PRIVATE_KEY;
-const alchemyUrl = process.env.ALCHEMY_URL;
-const etherscanApiKey = process.env.ETHERSCAN_API_KEY; // Ploygn Key.
-const coinmarketCapKey = process.env.COINMARKET_CAP_KEY;
+const {
+  rinkebyPrivateKey,
+  alchemyUrl,
+  etherscanApiKey,
+  coinmarketCapKey,
+} = require("./secretsManager.example.js");
 
 module.exports = {
   solidity: {
@@ -42,24 +40,19 @@ module.exports = {
   namedAccounts: {
     deployer: 0,
   },
-  networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true,
-      initialBaseFeePerGas: 0,
-    },
-    //Polygon Network
-    mumbai: {
-     url: alchemyUrl,
-     accounts: [`0x${polygonPrivateKey}`]
-   },
-    // rinkeby: {
-    //   url: alchemyUrl,
-    //   accounts: [`0x${rinkebyPrivateKey}`],
-    // },
-  },
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: etherscanApiKey,
-  },
+  // networks: {
+  //   hardhat: {
+  //     allowUnlimitedContractSize: true,
+  //     initialBaseFeePerGas: 0,
+  //   },
+  //   rinkeby: {
+  //     url: alchemyUrl,
+  //     accounts: [`0x${rinkebyPrivateKey}`],
+  //   },
+  // },
+  // etherscan: {
+  //   // Your API key for Etherscan
+  //   // Obtain one at https://etherscan.io/
+  //   apiKey: etherscanApiKey,
+  // },
 };
