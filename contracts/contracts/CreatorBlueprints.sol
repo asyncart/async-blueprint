5796a64333bc1c459c2f118fba2489bc1eb4a6b2
Sam O <sam.orend22@gmail.com>
2022-11-30 01:48:58 -0500

    default on in intialize

--- contracts/contracts/CreatorBlueprints.sol
+++ contracts/contracts/CreatorBlueprints.sol
@@ -272,14 +272,12 @@ contract CreatorBlueprints is
      * @param creatorBlueprintsAdmins Administrative accounts 
      * @param _royaltyParameters Initial royalty settings 
      * @param extraMinter Additional address to give minter role
-     * @param restrictOperators Whether or not to restrict transfers and approvals based on OpenSea's operator blacklist
      */
     function initialize(
         CreatorBlueprintsInput calldata creatorBlueprintsInput,
         IBlueprintTypes.Admins calldata creatorBlueprintsAdmins,
         RoyaltyParameters calldata _royaltyParameters,
-        address extraMinter,
-        bool restrictOperators
+        address extraMinter
     ) public initializer validRoyaltyParameters(_royaltyParameters) {
         // Intialize parent contracts
         ERC721Upgradeable.__ERC721_init(creatorBlueprintsInput.name, creatorBlueprintsInput.symbol);
@@ -302,10 +300,8 @@ contract CreatorBlueprints is
         contractURI = creatorBlueprintsInput.contractURI; 
         royaltyParameters = _royaltyParameters;
 
-        if (restrictOperators) {
-            operatorFilterRegistry = IOperatorFilterRegistry(0x000000000000AAeB6D7670E522A718067333cd4E);
-            operatorFilterRegistry.registerAndSubscribe(address(this), 0x3cc6CddA760b79bAfa08dF41ECFA224f810dCeB6);
-        }
+        operatorFilterRegistry = IOperatorFilterRegistry(0x000000000000AAeB6D7670E522A718067333cd4E);
+        operatorFilterRegistry.registerAndSubscribe(address(this), 0x3cc6CddA760b79bAfa08dF41ECFA224f810dCeB6);
     }
 
     /**
